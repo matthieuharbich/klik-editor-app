635840e780632ed88d9ee6845d206012e2df7d24
Matthieu Harbich <matthieu.harbich@gmail.com>
2015-08-01 12:54:28 +0200

    yep

--- client/assets/klikLib/klikEvent.js
+++ client/assets/klikLib/klikEvent.js
@@ -13,43 +13,49 @@ KlikPath.prototype.sideBox = function(){
 	// }else{
 	// 	this.video.pause();
 	// }
-	var id = _this.layersId[0].sideBox;
-	console.log(id)
-	$('#'+ id).empty();
 
-	if (this.polygon.element.name) {
-		var name = this.polygon.element.name;
-	}else{
-		var name = '';
-	};
 
-	if (this.polygon.element.description) {
-		var description = this.polygon.element.description;
-	}else{
-		var description = '';
-	};
 
-	if (this.polygon.element.price) {
-		var price = 'Price: ' +this.polygon.element.price + ' $';
-	}else{
-		var price = '';
-	};
+
+
+	//cette partie a été commentée a cause de layerId
+	// var id = this.layersId[0].sideBox;
+	// console.log(id)
+	// $('#'+ id).empty();
+
+	// if (this.polygon.element.name) {
+	// 	var name = this.polygon.element.name;
+	// }else{
+	// 	var name = '';
+	// };
+
+	// if (this.polygon.element.description) {
+	// 	var description = this.polygon.element.description;
+	// }else{
+	// 	var description = '';
+	// };
+
+	// if (this.polygon.element.price) {
+	// 	var price = 'Price: ' +this.polygon.element.price + ' $';
+	// }else{
+	// 	var price = '';
+	// };
 
 	
-	$('#'+ id).append('<button style="position:absolute; top: 10px; right: 10px;" id="closeBox"> X </button> <h1 style="text-align: center;">'+ name+'</h1><p style="font-size:1.2em; margin-left: 10px; margin-right: 10px;">'+description+'</p><h4 style="text-align:right; position:absolute; bottom:40px; font-size:1.3; margin-left:10px;">'+price+'</h4>');
+	// $('#'+ id).append('<button style="position:absolute; top: 10px; right: 10px;" id="closeBox"> X </button> <h1 style="text-align: center;">'+ name+'</h1><p style="font-size:1.2em; margin-left: 10px; margin-right: 10px;">'+description+'</p><h4 style="text-align:right; position:absolute; bottom:40px; font-size:1.3; margin-left:10px;">'+price+'</h4>');
 	
-	$('#'+ id).animate({
-		    left: "0%"
-		  }, 400, function() {
+	// $('#'+ id).animate({
+	// 	    left: "0%"
+	// 	  }, 400, function() {
 
-		  });
-	$('#closeBox').on('mousedown',function(){
-		$('#'+ id).animate({
-		    left: "-100%"
-		  }, 400, function() {
+	// 	  });
+	// $('#closeBox').on('mousedown',function(){
+	// 	$('#'+ id).animate({
+	// 	    left: "-100%"
+	// 	  }, 400, function() {
 
-		  });
-	})
+	// 	  });
+	// })
 	
 	
 }
